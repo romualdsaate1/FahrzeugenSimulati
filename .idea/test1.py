979a92e6270c87d7889924be5fa2ea8b0f97224b
romuald saate <rommualdsaate@gmail.com>
2018-04-11 15:17:53 +0200

    first message

--- .idea/test1.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from xml.etree.ElementTree import ElementTree
-tree = ElementTree()
-tree.parse("index.xml")
-p = tree.find("body/p")     # Finds first occurrence of tag p in body
-links = p.getiterator("a")  # Returns list of all links
-for i in links:
-    i.attrib["target"] = "blank"
-    tree.write("output.xml")     # Iterates through all found links
